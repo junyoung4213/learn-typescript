42dd135ccaddd046332aa6ed75b00f73fbbffbaf
junyoung4213 <junyoung4213@gmail.com>
2022-02-28 16:48:55 +0900

    타입 별칭과 인터페이스의 차이점

--- class-note/4_type-aliases.ts
+++ class-note/4_type-aliases.ts
@@ -1,12 +1,14 @@
-// interface Person {
+interface Person {
+  name: string;
+  age: number;
+}
+
+// type Person  = {
 //   name: string;
 //   age: number;
 // }
 
-type Person  = {
-  name: string;
-  age: number;
-}
+/* type은 확장이 불가능하나, interface는 extends로 확장이 가능하므로 가능하면 interface를 쓰도록 하자 */
 
 var seho: Person = {
   name: '세호',
@@ -18,5 +20,5 @@ var str: MyString = 'hello';
 
 type Todo = { id: string; title: string; done: boolean };
 function getTodo(todo: Todo) {
-  
+
 }
