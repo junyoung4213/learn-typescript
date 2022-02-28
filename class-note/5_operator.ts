2bfe773a4be72d5eee640454a6ebe20eca0dca68
junyoung4213 <junyoung4213@gmail.com>
2022-02-28 17:02:32 +0900

    유니온 타입의 특징

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -17,3 +17,18 @@ function logMessage(value: string | number) { // 하나의 타입 이상을 쓸 
 
 logMessage('hello');
 logMessage(100);
+
+interface Developer {
+  name: string;
+  skill: string;
+}
+
+interface Person {
+  name: string;
+  age: number;
+}
+function askSomeone(someone: Developer | Person) {
+  someone.name;
+  someone.skill;  // 위에서 유니온 타입으로 Developer, Person을 정의했지만 skill과 age는 불러올 수 없다.
+  someone.age;  // 유니온 타입으로 정의했다 하더라도, 인터페이스간에 중복된 값만 불러올 수 있는데 이는 보장된 속성이 아닌 것을 불러오려고 하면 에러 발생 위험이 있기 때문이다.
+}
