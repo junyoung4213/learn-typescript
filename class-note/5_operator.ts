ccee51086f8b35f49c96c2792d67ac7a54b74fba
junyoung4213 <junyoung4213@gmail.com>
2022-02-28 16:57:19 +0900

    유니온 타입의 장점

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -4,8 +4,15 @@
 // logMessage('hello');
 // logMessage(100);
 
+var seho: string | number | boolean;
 function logMessage(value: string | number) { // 하나의 타입 이상을 쓸 수 있게 하는 Union Type
-  console.log(value);
+  if(typeof value === 'number'){ // number 타입일 경우, value의 타입을 number로 인식하여 인텔리센스 작동.
+    value.toLocaleString();
+  }
+  if(typeof value === 'string'){ // string 타입일 경우, value의 타입을 string으로 인식하여 인텔리센스 작동.
+    value.toString();
+  }
+  throw new TypeError('value must be string or number');
 }
 
 logMessage('hello');
