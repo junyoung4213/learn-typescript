95890040f4683a302aea001f45b55511648d691c
junyoung4213 <junyoung4213@gmail.com>
2022-02-28 16:37:46 +0900

    인터페이스 딕셔너리 패턴

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -35,4 +35,20 @@ interface StringArray {
 }
 
 var arr: StringArray = ['a','b', 'c'];
-arr[0] = 10;
+// arr[0] = 10;
+
+// 딕셔너리 패턴
+interface StringRegexDictionary {
+  [key: string]: RegExp
+}
+
+var obj: StringRegexDictionary = {
+  // sth: /abc/,
+  cssFile: /\.css$/,
+  jsFile: /\.js$/,
+}
+// obj['cssFile'] = 'a' 
+
+Object.keys(obj).forEach(function(value) {
+
+})
