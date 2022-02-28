3363fab046b2930645a524d61d68f00c10a5cb21
junyoung4213 <junyoung4213@gmail.com>
2022-02-28 14:48:36 +0900

    튜플, 객체, 진위값 사용방법

--- class-note/1_type-basic.ts
+++ class-note/1_type-basic.ts
@@ -11,3 +11,21 @@ let num: number = 10;
 let arr: Array<number> = [1,2,3];
 let items: number[] = [1,2,3]; // 위와 동일
 let heroes: Array<string> = ['Capt', 'Thor', 'Hulk']
+
+// TS 튜플
+
+let address: [string, number] = ['gangnam', 1]; // 배열의 각각 인덱스에 타입을 정의
+
+// TS 객체
+let obj: object = {};
+// let person: object = { // 객체를 단순하게 정의할 때
+//   name: 'capt',
+//   age: 100
+// };
+let person: { name: string, age: number} = { // 객체를 구체적으로 정의할 때
+  name: 'thor',
+  age: 1000
+}
+
+// TS 진위값
+let show: boolean = false;
