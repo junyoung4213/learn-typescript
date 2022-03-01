0126354ba8bf16bd9acb9708d018e2b1b32fcb50
junyoung4213 <junyoung4213@gmail.com>
2022-03-01 21:58:16 +0900

    함수 중복 선언의 단점

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -6,9 +6,27 @@
 // logText('test'); // 문자열 test
 // logText(10); // 숫자 10
 
-function logText<T>(text: T):T {
+// function logText<T>(text: T):T {
+//   console.log(text);
+//   return text;
+// }
+
+// logText<string>('하이');
+
+
+/* 단순히 맞는 타입을 받기 위해 중복으로 함수를 선언하게 되서 비효율적이게 된다. */
+function logText(text: string) {
   console.log(text);
+  // text.split('').reverse().join('');
   return text;
 }
 
-logText<string>('하이');
+function logNumber(num: number) {
+  console.log(num);
+  return num;
+}
+
+logText('a');
+logText(10);
+const num = logNumber(10);
+logText(true);
